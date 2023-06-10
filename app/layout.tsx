3b6f944783026adae6bb03009b3a4ef775be2563
kotzimop <kotzimop@gmail.com>
2023-06-10 11:14:36 +0300

    Rapid API cars & Car Details Modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,24 @@
-import { Footer, NavBar } from '@/components'
-import './globals.css'
-import { Inter } from 'next/font/google'
-
-
+import { Footer, NavBar } from "@/components";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
 export const metadata = {
-  title: 'Car Hub',
-  description: 'Discover the best cars in the world',
-}
+  title: "Car Hub",
+  description: "Discover the best cars in the world",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className='relative'>
+      <body suppressHydrationWarning={true} className="relative">
         <NavBar />
         {children}
         <Footer />
-        </body>
+      </body>
     </html>
-  )
+  );
 }
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,7 +4,13 @@ import Image from 'next/image'
 import { CustomButtonProps } from '@/types'
 
 const CustomButton:React.FC<CustomButtonProps> = ({
-    title,containerStyles,handleClick, btnType
+    title,
+    containerStyles,
+    handleClick, 
+    btnType, 
+    textStyles, 
+    rightIcon, 
+    isDisabled
 }) => {
   return (
    <button
@@ -13,7 +19,12 @@ const CustomButton:React.FC<CustomButtonProps> = ({
    className={`custom-btn ${containerStyles}`}
    onClick={handleClick}
    >
-    <span className={`flex-1`}>{title}</span>
+    <span className={`flex-1 ${textStyles}`}>{title}</span>
+   {rightIcon && (
+    <div className='relative w-6 h-6'>
+      <Image src={rightIcon} alt='right icon' fill className='object-contain'/>
+    </div>
+   )}
    </button>
   )
 }
